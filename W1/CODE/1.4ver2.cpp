21779ffcd40d9528cd512eee297cbb2997a37808
kynestic <148428803+kynestic@users.noreply.github.com>
2024-05-31 19:42:46 +0700

    update

--- W1/CODE/1.4ver2.cpp
+++ W1/CODE/1.4ver2.cpp
@@ -1,58 +1,60 @@
 #include<iostream>
 #include<stack>
-#include<vector>
 #include<algorithm>
 
 using namespace std;
 
-int m, n;
+int m, n, ans = 0;
+int** arr;
+int* histogram;
 
-int findRecentMax(vector<int>& height) {
-    stack<int> st;
-    height.push_back(0); // Thêm ph?n t? 0 d? x? lý d? dàng hon
-    int maxS = 0;
-    for (int i = 0; i <= n; i++) { // Duy?t t? 0 d?n n (do height.push_back(0) nên kích thu?c là n+1)
-        while (!st.empty() && height[st.top()] > height[i]) {
-        	cout << height[st.top()] << " " << height[i]<< endl;
-            int h = height[st.top()];
-            st.pop();
-            int width = (st.empty()) ? i : i - 1 - st.top();
-            maxS = max(maxS, width * h);
-        }
-        st.push(i);
-    }
-    height.pop_back(); // Lo?i b? ph?n t? 0 dã thêm vào
-    return maxS;
+int findMaxRecent(){
+	int tmpAns = 0;
+	stack<int> st;
+	
+	for (int i = 1; i <= n + 1; i++){
+		while (!st.empty() && histogram[st.top()] > histogram[i]){
+			int tmp = st.top();
+			st.pop();
+			int width = !st.empty() ? i - st.top() - 1: i - 1;
+			tmpAns = max(tmpAns, histogram[tmp]* width);
+			//cout << i <<"-"<<tmp <<"-" << histogram[tmp] << "-"<<width <<" ";
+		}
+		
+		
+		st.push(i);
+	}
+	//cout <<tmpAns<< endl;
+	
+	return tmpAns;
 }
 
-void findMax(vector<vector<int>>& arr) {
-    if (arr.empty()) return;
-    int maxS = 0;
-    vector<int> height(n, 0); // Kh?i t?o m?ng height v?i kích thu?c n
-    
-    for (int i = 0; i < m; i++) { // Duy?t t? 0 d?n m-1
-        for (int j = 0; j < n; j++) { // Duy?t t? 0 d?n n-1
-            height[j] = (arr[i][j] == 1) ? height[j] + 1 : 0;
-        }
-        maxS = max(maxS, findRecentMax(height));
-    }
-        
-    cout << maxS;
-    return;
+void findMax(){
+	for (int i = 1; i <= m; i++){
+		for (int j = 1; j <= n; j++)
+			histogram[j] = arr[i][j] == 0 ? 0 : histogram[j] + 1; 
+		ans = max(ans, findMaxRecent());
+	}
 }
 
-int main() {
-    cin >> m >> n;
-    vector<vector<int>> arr(m, vector<int>(n)); // Kh?i t?o m?ng 2D v?i kích thu?c m x n
-    
-    for (int i = 0; i < m; i++) { // Duy?t t? 0 d?n m-1
-        for (int j = 0; j < n; j++) { // Duy?t t? 0 d?n n-1
-            cin >> arr[i][j];
-        }
-    }
-        
-    findMax(arr);
-    
-    return 0;
+int main(){
+	cin >> m >> n;
+	arr = new int*[m + 1];
+	histogram = new int[n + 2];
+	
+	for (int i = 1; i <= m; i++)
+		arr[i] = new int[n + 1];
+		
+	for(int i = 1; i <= m; i++)
+		for (int j = 1; j <= n; j++)
+			cin >> arr[i][j];
+			
+	for (int i = 1; i <= n + 1; i++)
+		histogram[i] = 0;
+	
+	findMax();
+	
+	cout << ans;
+	
+	return 0;
 }
-
